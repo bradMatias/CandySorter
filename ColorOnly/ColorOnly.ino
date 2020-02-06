d75e645360f03cbf4ae96cc32388cc50ab780f80
Brad <bqm111@gmail.com>
2020-02-05 17:33:04 -0800

    Adding color only file

--- ColorOnly/ColorOnly.ino
+++ ColorOnly/ColorOnly.ino
@@ -1,24 +1,10 @@
-#include <Servo.h> 
 #include <Adafruit_TCS34725.h>
 #include <FastLED.h> // use version 3.3.0
 
-#define SERVO_PIN 9
-#define SERVO2_PIN 10
-#define DROP_ANG 10
-#define COLOR_ANG 110
-#define TOP_ANG 160
-
-Servo myservo;
-Servo myservo2;
-// TCS34725_INTEGRATIONTIME_700MS
 Adafruit_TCS34725 colorSensor = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_1X                                                  );
 
 void setup() {
-  myservo.attach( SERVO_PIN, 560, 2540 );
-  myservo2.attach( SERVO2_PIN, 560, 2540 );
   Serial.begin( 9600 );
-  myservo.write( TOP_ANG );
-  delay( 2000 );
   
   if (colorSensor.begin()) {
     Serial.println( "Found sensor" );
@@ -32,16 +18,12 @@ float r, g, b;
 char buff[20];
 
 void loop() {
-  myservo.write( COLOR_ANG );
-  delay( 1000 );
   colorSensor.getRGB( &r, &g, &b );
   sprintf( buff, "%d %d %d", (byte)r, (byte)g, (byte)b );
-  CRGB rgb( r, g, b );
-  CHSV hsv = rgb2hsv_approximate( rgb );
-  //Serial.println( hsv.h );
   Serial.println(buff);
-  myservo.write( DROP_ANG );
-  delay( 500 );
-  myservo.write( TOP_ANG );
-  delay( 2000 );
+
+  // Bonus code!
+  //CRGB rgb( r, g, b );
+  //CHSV hsv = rgb2hsv_approximate( rgb );
+  //Serial.println( hsv.h );
 }
